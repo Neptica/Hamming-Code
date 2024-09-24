426b8af69c4cca1343946f0b6c6a62ff09398448
Michael Mumme <michaelmumme@me.com>
2024-09-24 16:15:59 -0500

    add hamming code to words
    start at 64 bits add 7 bits of ecc and end with 71 per grouping

--- functions.cpp
+++ functions.cpp
@@ -14,28 +14,65 @@ std::string Functions::input_string() {
     word += std::bitset<8>(input[i]).to_string();
   }
   return word;
-}
+} // asdflkjaslkdfjlaksdjflaksjdflaksjdflaksdjdfjlaksdjdfj
 
 std::string Functions::pad(std::string word) {
   /* start of data is a NULL ASCII character everything before the NULL char is
    * a string of ones */
-  if (word.size() == 48)
+  if (word.size() == 56)
     return "00000000" + word;
-  int preamble_size = (word.size() / 48 + 1) * 56 - word.size();
+  int preamble_size = (word.size() / 48 + 1) * 64 - word.size();
   return std::string(preamble_size - 8, '1') + std::string(8, '0') + word;
 }
 
 std::string Functions::hammit(std::string word) {
   /* this creates error correcting code for later use */
   int current_multiplier = 1;
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 7; i++) {
     word.insert(current_multiplier - 1, "0");
     current_multiplier *= 2;
   }
-  // for (int i = 0; i < 6; i++)
-  //   int checking = current_multiplier;
-  //   std::cout << checking << "\n";
-  //   current_multiplier *= 2;
-  // }
-  return word + "00";
+  bool ones = false;
+  bool twos = false;
+  bool fours = false;
+  bool eights = false;
+  bool sixteens = false;
+  bool thirty_twos = false;
+  bool sixty_fours = false;
+  for (int i = 0; i < 71; i++) {
+    if (word[i] == 0)
+      continue;
+    int pos = i + 1;
+    int number = (int)word[i] % 2;
+    if (pos % 2 == 1 && word[i] == '1')
+      ones = !(ones);
+    if ((pos / 2) % 2 == 1 && word[i] == '1')
+      twos = !(twos);
+    if ((pos / 4) % 2 == 1 && word[i] == '1')
+      fours = !(fours);
+    if ((pos / 8) % 2 == 1 && word[i] == '1')
+      eights = !(eights);
+    if ((pos / 16) % 2 == 1 && word[i] == '1')
+      sixteens = !(sixteens);
+    if ((pos / 32) % 2 == 1 && word[i] == '1')
+      thirty_twos = !(thirty_twos);
+    if ((pos / 64) % 2 == 1 && word[i] == '1')
+      sixty_fours = !(sixty_fours);
+  }
+  std::cout << "word before hamming : " << word << "\n";
+  if (ones)
+    word[0] = '1';
+  if (twos)
+    word[1] = '1';
+  if (fours)
+    word[3] = '1';
+  if (eights)
+    word[7] = '1';
+  if (sixteens)
+    word[15] = '1';
+  if (thirty_twos)
+    word[31] = '1';
+  if (sixty_fours)
+    word[63] = '1';
+  return word;
 }
--- main.cpp
+++ main.cpp
@@ -8,18 +8,9 @@ int main() {
   std::string padded_word = Functions::pad(word);
 
   std::string codeword = "";
-  int i;
-  for (i = 0; i < padded_word.size(); i += 56) {
-    std::string substring = padded_word.substr(i, 56);
-    // std::cout << " this is the substr : " << substring
-    //           << " and this is its size " << substring.size();
+  for (int i = 0; i < padded_word.size(); i += 64) {
+    std::string substring = padded_word.substr(i, 64);
     codeword += Functions::hammit(substring);
   }
-  // for (int i = 0; i < codeword.size(); i++) {
-  //   std::cout << (i + 1) % 64 << ": " << codeword[i] << "\n";
-  // }
-  // if (i + 56 < bit_count) {
-  //   codeword += Functions::hammit(word.substr(i, bit_count - 1));
-  // }
-  // std::cout << codeword.size() << "\n";
+  std::cout << "word after hamming  : " << codeword << "\n";
 }
